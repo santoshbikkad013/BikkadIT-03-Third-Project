7fbc0b75b1865e7bab6d872ae1643efbb2ae07d6
santoshbikkad013 <santoshbikkad013@gmail.com>
2022-08-04 09:53:10 +0530

    SMS-03: STudentServiceImpl class added

---
+++ src/main/java/com/example/demo/serviceI/StudentServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.demo.serviceI;
+
+import java.util.List;
+
+import com.example.demo.model.Student;
+
+public class StudentServiceImpl implements StudentServiceI {
+
+	@Override
+	public int addStudent(Student student) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Student> getAllStudent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getStudentById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
