592d3b1caef11316d3f3eff659d7b6f967460249
santoshbikkad013 <santoshbikkad013@gmail.com>
2022-08-05 10:32:23 +0530

    StudentServiceImpl Implemention completed

--- src/main/java/com/example/demo/service/StudentServiceI.java
+++ src/main/java/com/example/demo/service/StudentServiceI.java
@@ -11,6 +11,6 @@ public interface StudentServiceI {
 	
 	public List<Student> getAllStudent();
 	
-	public Student getStudentById();
+	public Student getStudentById(int id);
 	
 }
--- src/main/java/com/example/demo/service/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/StudentServiceImpl.java
@@ -1,27 +1,35 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.model.Student;
+import com.example.demo.reposoitory.StudentRepository;
 
 public class StudentServiceImpl implements StudentServiceI{
 
+	@Autowired
+	private StudentRepository studentRepository;
+	
 	@Override
 	public int addStudent(Student student) {
-		// TODO Auto-generated method stub
-		return 0;
+		Student save = studentRepository.save(student);
+		
+		return save.getStudentId();
 	}
 
 	@Override
 	public List<Student> getAllStudent() {
-		// TODO Auto-generated method stub
-		return null;
+	 List<Student> findAll = studentRepository.findAll();
+		return findAll;
 	}
 
 	@Override
-	public Student getStudentById() {
-		// TODO Auto-generated method stub
-		return null;
+	public Student getStudentById(int id) {
+		Student findById = studentRepository.findById(id).get();
+		return findById;
 	}
 
 }
